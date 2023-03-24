a54e3f6db9e835851d16be274f137632e0b94b4f
Yofret Rios <yofret.rios@firstleaf.com>
2023-03-24 12:43:31 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `canonical-test`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
